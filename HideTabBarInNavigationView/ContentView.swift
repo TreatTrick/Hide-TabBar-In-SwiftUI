70e9e156890997f0aed58b0670d229005aa53cf2
TreatTrick <1154174767@qq.com>
2020-10-17 11:56:30 +0800

    Initial Commit

---
+++ HideTabBarInNavigationView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HideTabBarInNavigationView
+//
+//  Created by SORA on 2020/10/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HideTabBarInNavigationView/HideTabBarInNavigationViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  HideTabBarInNavigationViewApp.swift
+//  HideTabBarInNavigationView
+//
+//  Created by SORA on 2020/10/17.
+//
+
+import SwiftUI
+
+@main
+struct HideTabBarInNavigationViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
